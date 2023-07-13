530225a09b1d634bf290658d8f810c6a8d9c7a1d
JulisaJ10 <55122258+JulisaJ10@users.noreply.github.com>
2023-07-12 18:05:58 -0600

    grafica pastel

---
+++ funcionalidades.js
@@ -0,0 +1,23 @@
+function enlazar(){
+    var endpoint= document.getElementById("enpoint") .value;
+    fetch(endpoint)
+    .then(function(respuesta){
+        return respuesta.json();
+    })
+
+    .then(function(datos){
+        console.log(datos);
+        var nombre=[];
+        var habilidad=[];
+        var fuerza=[];
+        for(var i = 0; i < datos.length; i++){
+            if((datos[i].category!= undefined) && (datos[i]name!=undefined)){
+                nombre.push(datos[i].name);
+                habilidad.push(datos[i].hability);
+                fuerza.push(datos[i].fuerza);
+
+            }
+        }
+        alert(name);
+    })
+}
